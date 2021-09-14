eb168418663413741ea7a6be7de9fd01419594ee
OwenStadlwieser <47876306+OwenStadlwieser@users.noreply.github.com>
2021-09-14 16:00:31 -0600

    Update salaryAndPositionWebScraping.js

--- webscraping/salaryAndPositionWebScraping.js
+++ webscraping/salaryAndPositionWebScraping.js
@@ -3,65 +3,91 @@ import excel4node from 'excel4node'
 // add stealth plugin and use defaults (all evasion techniques)
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 import fs, { stat } from 'fs'
+import AdblockerPlugin  from 'puppeteer-extra-plugin-adblocker'
 
 (async () => {
-    let rawdata = fs.readFileSync('json/nflWeek1BearsVsRams.json');
-    let players = JSON.parse(rawdata);
-    console.log(players);
+    // let rawdata = fs.readFileSync('json/nflWeek1BearsVsRams.json');
+    // let players = JSON.parse(rawdata);
+    // console.log(players);
+    let players = {}
     puppeteer.use(StealthPlugin())
+    puppeteer.use(AdblockerPlugin())
     const browser = await puppeteer.launch({headless: false});
     const context = browser.defaultBrowserContext()
     await context.overridePermissions("https://chercher.tech/practice/geo-location", ['geolocation'])
     const page = await browser.newPage()
     await page.setGeolocation({latitude: 53.544388, longitude: -113.490929});
     await page.goto('https://www.fantasypros.com/daily-fantasy/nfl/fanduel-salary-changes.php');
-
-    const positions = ['QB', 'RB', 'WR', 'TE', 'DST']
+    await page.select('.site-select', '/daily-fantasy/nfl/draftkings-salary-changes.php')
+    const positions = ['QB', 'RB', 'WR', 'TE']
     for (let position = 0; position < positions.length; position++) {
-        await page.waitForXPath(`//tr[contains(@class, "${positions[position]}")]/td[2]/a`, { timeout: 10000 })
-        const el = await page.$x(`//tr[contains(@class, "${positions[position]}")]/td[2]/a`)
-        for(let i =0; i< el.length; i = i +2) {
-            let textContent = await (await el[i].getProperty('textContent')).jsonValue()
-            let stringValue = new String(textContent)
-            stringValue = stringValue.toString()
-            stringValue = stringValue.replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "");
-            if(players[stringValue]) players[stringValue].position = positions[position]
-            else continue
-            const salary = await page.$x(`//tr[contains(@class, "${positions[position]}")]/descendant::td[contains(@class, "salary")]`)
-            let textContentSal = await (await salary[i / 2].getProperty('textContent')).jsonValue()
-            let stringValueSal = new String(textContentSal)
-            stringValueSal = stringValueSal.toString()
-            stringValueSal = stringValueSal.replace(/[^a-z0-9]/gmi, "")
-            players[stringValue].salary = parseInt(stringValueSal)
-            if( parseInt(stringValueSal)===0) console.log(stringValue, 'Salary Error')
-            // await el[i].click()
-            // const statsArray = []
-            // const stats =await page.$x('//table[contains(@class, "table table-bordered all-stats")]//tbody/tr/td')
-            // for (let j = 0; j < stats.length; j++) {
-            //     let textContent2 = await (await stats[i].getProperty('textContent')).jsonValue()
-            //     let stringValue2s = new String(textContent2)
-            //     stringValue2 = stringValue2.toString()
-            //     statsArray.push(stringValue2)
-            // }
-            // if(statsArray.length === 10) players[stringValue].fantasyPros = { 
-            //     passCmp: statsArray[0],
-            //     passAttp: statsArray[1],
-            //     passYds: statsArray[2],
-            //     passTds: statsArray[3],
-            //     passInts: statsArray[4],
-            //     rushAtt: statsArray[5],
-            //     rushYds: statsArray[6],
-            //     rushTds: statsArray[7],
-            //     fumbles: statsArray[8],
-            //     points: statsArray[9]
-            // }
+        await page.waitForXPath(`//tr[contains(@class, "${positions[position]}")]/td[2]/a`, { timeout: 3000 })
+        const el = await page.$x(`//tr[contains(@class, "${positions[position]}")]/td[2]/a[1]`)
+        for(let i =0; i< el.length; i++) {
+            try {
+                let findElement =  await page.$x(`//tr[contains(@class, "${positions[position]}")][${i}]/td[2]/a[1]`)
+                let textContent = await (await findElement[0].getProperty('textContent')).jsonValue()
+                let stringValue = new String(textContent)
+                stringValue = stringValue.toString()
+                stringValue = stringValue.replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "");
+                const team = await page.$x(`//tr[contains(@class, "${positions[position]}")][${i}]/td[2]/small`)
+                const teamTextContent = await (await team[0].getProperty('textContent')).jsonValue()
+                let stringValueTeam = new String(teamTextContent)
+                stringValueTeam = stringValueTeam.toString()
+                stringValueTeam = stringValueTeam.split(" ", 1)
+                stringValueTeam = stringValueTeam[0].replace(/[^a-z0-9]/gmi, "").replace(/\s+/g, "");
+                if(!players[stringValue]) players[stringValue] = {}
+                players[stringValue].position = positions[position]
+                players[stringValue].team = stringValueTeam
+                console.log(stringValueTeam)
+                const salary = await page.$x(`//tr[contains(@class, "${positions[position]}")][${i}]/descendant::td[contains(@class, "salary")]`)
+                let textContentSal = await (await salary[0].getProperty('textContent')).jsonValue()
+                let stringValueSal = new String(textContentSal)
+                stringValueSal = stringValueSal.toString()
+                stringValueSal = stringValueSal.replace(/[^a-z0-9]/gmi, "")
+                console.log(stringValue)
+                console.log(stringValueSal)
+                players[stringValue].salary = parseInt(stringValueSal)
+                if( parseInt(stringValueSal)===0) console.log(stringValue, 'Salary Error')
+
+                let href = await (await el[i].getProperty('href')).jsonValue()
+                let hrefString = new String(href)
+                hrefString = hrefString.toString()
+                const playerPage = await browser.newPage()
+                await playerPage.goto(hrefString);
+                // await el[i].click()
+                // const statsArray = []
+                const statsObject = {}
+                try {
+                    await playerPage.waitForXPath('//table[contains(@class, "table table-bordered all-stats")]//tbody/tr[1]/td', { timeout: 2000 })
+                    const stats = await playerPage.$x('//table[contains(@class, "table table-bordered all-stats")]//tbody/tr[1]/td')
+                    const statsTitles = await playerPage.$x('//table[contains(@class, "table table-bordered all-stats")]//thead/tr/th')
+                    for (let j = 0; j < stats.length; j++) {
+                        let statContent = await (await stats[j].getProperty('textContent')).jsonValue()
+                        let statContentString = new String(statContent)
+                        statContentString = statContentString.toString()
+                        let titleContent = await (await statsTitles[j].getProperty('textContent')).jsonValue()
+                        let title = new String(titleContent)
+                        title = title.toString()
+                        statsObject[title] = parseFloat(statContentString)
+                    }
+                } catch(err) { console.log('Stats Error', stringValue, err) }
+                players[stringValue] = { ...players[stringValue], ...statsObject}
+                await playerPage.close()
+            }
+            catch(err) {
+                console.log(i)
+                console.log(el.length)
+                console.log(err)
+            }
         }
     }
-    fs.writeFile("json/nflWeek1WithSalariesChiLa.json", JSON.stringify(players), function(err) {
+    fs.writeFile("json/nflWeek2WithProjectedPoints.json", JSON.stringify(players), function(err) {
         if (err) {
             console.log(err);
         }
       });
+    console.log('ALL DONE')
     let obj
     let teamsAlreadySeen = []
     let allPlayers = {}
